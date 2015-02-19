03903b44b23afc26147ab3eb4bd2abbbf4c4f6f3
Ryan Hartman <ryandavidhartman@gmail.com>
2015-02-18 19:22:29 -0500

    Implementing OrderService

--- src/Microservice.Host/OrderService.cs
+++ src/Microservice.Host/OrderService.cs
@@ -1,8 +1,43 @@
-﻿using ServiceStack;
+﻿using System.Collections.Generic;
+using ServiceStack;
 
 namespace Microservice.Host
 {
     public class OrderService : Service
     {
+        private List<Order> _orders;
+
+        public OrderService()
+        {
+            _orders = new List<Order>();
+        }
+
+        public object Get(GetOrders request)
+        {
+            return new List<Order>();
+        }
+
+        public object Post(Order data)
+        {
+            var id = _orders.Count + 1;
+            data.Id = id;
+            _orders.Add(data);
+            return data;
+        }
+
+        public object Put(Order data)
+        {
+            var index = _orders.FindIndex(i => i.Id == data.Id);
+            _orders[index] = data;
+            return data;
+        }
+
+        public object Delete(Order data)
+        {
+            var order = _orders.Find(i => i.Id == data.Id);
+            _orders.Remove(order);
+            return null;
+        }
+
     }
 }
